bfadac4e11612875054c8dc01a583d6d11c7146d
Jaidan Vang <111729028+jaidanvang@users.noreply.github.com>
2023-03-20 17:02:41 +0000

    Mon, Mar 20, 2023, 10:02 AM -07:00

--- js/jokes.js
+++ js/jokes.js
@@ -1,2 +1,13 @@
 const jokeE1 = document.getElementById('joke')
 const jokebtn = document.getElementById('jokeBtn')
+
+async jokeBtn.addEventListener('click', generateJoke)
+
+ascyn function generateJoke(){
+    const res = await fetch('https://icanhazdadjoke.com', {
+        headers: {Accept: 'application/json',},
+    })
+
+    const = data = await res.json()
+    jokeEl.innerHTML = data.joke
+}
