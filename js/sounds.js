7a1d48680b490ee5f7b9a42557fe882b2497fa26
Jaidan Vang <111729028+jaidanvang@users.noreply.github.com>
2023-03-20 16:32:41 +0000

    Mon, Mar 20, 2023, 9:32 AM -07:00

---
+++ js/sounds.js
@@ -0,0 +1,22 @@
+constn sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText = sound
+    btn.addEvenListener('click', () => {
+        stopSongs()
+        document.getElementBy(sound).play()
+    })
+    document.geElementById('buttons').appendChild(btn)
+})
+
+function stopSongs(){
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+        song.pause(
+        song.currentTime = 0
+        )
+    })
+}
+}
