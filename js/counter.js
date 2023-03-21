e2f735c622d710cb8b4e3c7977eb41b34e2bbb38
Jaidan Vang <111729028+jaidanvang@users.noreply.github.com>
2023-03-21 17:10:07 +0000

    Tue, Mar 21, 2023, 10:10 AM -07:00

--- js/counter.js
+++ js/counter.js
@@ -3,5 +3,5 @@ const counters = document.querySelectorAll('.counter')
 counters.forEach(counter=> {
     counter.innerText = '0'
 
-    const update counter
+    const updateCounter = +counter.getAttributes
 })
