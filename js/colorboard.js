e58ace8bae4358a7a98ad9d5fc9e2eed63d49a18
Jaidan Vang <111729028+jaidanvang@users.noreply.github.com>
2023-03-22 16:43:30 +0000

    Wed, Mar 22, 2023, 9:43 AM -07:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -1,11 +1,11 @@
 const container = document.getElementById('container')
-const colors = ['#e74c3c','#8e44ad','3498db','#e67e22','2ecc71']
+const colors = ['#e74c3c','#8e44ad','#3498db','#e67e22','2ecc71']
 const SQUARES = 500
 
 for(let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
     square.classlist.add('square')
-    container.appenChild(square)
+    container.appendChild(square)
     square.addEventListener('mouseover', () => setColor(square))
     square.addEventListener('mouseout', () => removeColor(square))
 }
