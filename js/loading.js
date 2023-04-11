1b9bf530faaef32a7ee4048804f576f640a8c776
Jaidan Vang <111729028+jaidanvang@users.noreply.github.com>
2023-04-11 05:24:44 +0000

    Mon, Apr 10, 2023, 10:24 PM -07:00

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,5 @@
 const bg = document.querySelector('.bg')
 const loadText = document.querySelector('.loading-text')
 
-bg.style.filter = "blur(5px)"
+bg.style.filter = "blur(1px)"
 loadText.innerText = "HELLO"
