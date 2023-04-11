8095247e45dd05fd8c1d659b70b616adf7353ceb
Jaidan Vang <111729028+jaidanvang@users.noreply.github.com>
2023-04-11 06:17:09 +0000

    Mon, Apr 10, 2023, 11:17 PM -07:00

--- js/expanding.js
+++ js/expanding.js
@@ -2,6 +2,15 @@ const panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
+        removeActive()
         panel.classList.add('active')
     })
 })
+
+
+
+function removeActive() {
+    panels.forEach(panel => {
+        [anel.classList.remove('active')
+    })
+}
