cc19e3974533d4aeaf0d86ef5cf302f3579ff0d1
Jaidan Vang <111729028+jaidanvang@users.noreply.github.com>
2023-04-11 05:32:07 +0000

    Mon, Apr 10, 2023, 10:32 PM -07:00

--- js/loading.js
+++ js/loading.js
@@ -1,1 +1,18 @@
+cons bg = document.querySelector('.bg')
+const loadText = document.querySelector('.loading-text')
 
+let load = 0
+
+let int =  setInterval(blurring, 30)
+
+function blurring(){
+    load++
+    loadText.innerText = ${load}%
+    if (load == 100) {
+        clearInterval(int)
+    }
+
+
+    bg.style.filter = 'blur(${30 - 0.3*load}px)'
+    loadText.style.opacity = '${1-load/100}'
+}
