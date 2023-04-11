3709f6d421059bfccbd63dd3862673cf6d5bdafb
Jaidan Vang <111729028+jaidanvang@users.noreply.github.com>
2023-04-11 05:22:08 +0000

    Mon, Apr 10, 2023, 10:22 PM -07:00

--- js/loading.js
+++ js/loading.js
@@ -1,18 +1,5 @@
 const bg = document.querySelector('.bg')
 const loadText = document.querySelector('.loading-text')
 
-let load = 0
-
-let int =  setInterval(blurring, 30)
-
-function blurring(){
-    load++
-    loadText.innerText = ${load}%
-    if (load == 100) {
-        clearInterval(int)
-    }
-
-
-    bg.style.filter = 'blur(${30 - 0.3*load}px)'
-    loadText.style.opacity = '${1-load/100}'
-}
+bg.style.filter = "blur(5px)"
+loadText.innerText = "HELLO"
