02eddc330782c3384e3f1cff0a3c4a09bba208a1
Jaidan Vang <111729028+jaidanvang@users.noreply.github.com>
2023-05-31 01:01:43 +0000

    Tue, May 30, 2023, 6:01 PM -07:00

--- js/loading.js
+++ js/loading.js
@@ -7,12 +7,11 @@ let int = setInterval(blurring, 30)
 
 function blurring(){
     load++
-    loadText.innerText = ${load}%
+    loadText.innerText = '${load}%'
     if (load == 100) {
         clearInterval(int)
     }
 
-    bg.style.filter = 'blur(${30 - 0.3*load}px)'
-    loadText.style.opacity = '${1-load/100}'
+    bg.style.filter = "blur(${30 - 0.3*load}px)"
 
 }
